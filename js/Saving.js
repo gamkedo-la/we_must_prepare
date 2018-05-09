b50984ede8e6b7cfde99be880ce79f80fd181647
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-05-09 14:37:34 -0400

    Implement auto-save interval

--- js/Saving.js
+++ js/Saving.js
@@ -1,3 +1,5 @@
+var autoSaveInterval = null;
+
 var autoSaveEnabled = persistence.getValue('autoSaveEnabled', false);
 // TODO: implement getBool/setBool
 if (autoSaveEnabled === null) {
@@ -10,6 +12,7 @@ function keyPressForSaving(evt) {
     persistence.setValue('autoSaveEnabled', autoSaveEnabled);
     if (autoSaveEnabled) {
       HTMLLog("auto-save enabled!");
+      activateAutoSave();
     }
     else {
       HTMLLog("auto-save disabled!");
@@ -17,8 +20,21 @@ function keyPressForSaving(evt) {
   }
 }
 
+function activateAutoSave() {
+  autoSaveInterval = setInterval(autoSave, 10 * 1000);
+}
+
+function deactivateAutoSave() {
+  clearInterval(autoSaveInterval);
+}
+
+function autoSave() {
+  HTMLLog("saving...");
+}
+
 if (autoSaveEnabled) {
   HTMLLog("auto-save enabled!");
+  activateAutoSave();
 }
 else {
   HTMLLog("auto-save disabled!");
