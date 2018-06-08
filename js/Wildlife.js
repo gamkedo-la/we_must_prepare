9790755cd884c7a30d4f7e78922dc8b00c52eb8f
McFunkypants <breakdance@mcfunkypants.com>
2018-06-07 18:51:59 -0700

    wildlife system: more than one animal supported

--- js/Wildlife.js
+++ js/Wildlife.js
@@ -7,43 +7,50 @@ var wildlife = (function () {
     const SPRITE_SIZE = 16;
     const ANIM_FRAMES = 8;
     const ANIM_SPEED = 8;
+    const NUM_BIRDS = 2; //100; lol SWARM!!!
 
-    var birdx = 999999999; // force a respawn
-    var birdy = 999999999;
+    var birdx = [];
+    var birdy = [];
+    var birdSpeedX = [];
+    var birdSpeedY = [];
     const RESPAWN_DISTANCE_OFFSCREEN = 2000; // extra so the bird isn't always on screen    
-    var flySpeedX = 0.5;
-    var flySpeedY = 0;
     var altitudeVariance = 20;
 
     this.draw = function (cameraOffsetX, cameraOffsetY) {
 
         frameCount++;
 
-        // fly forward
-        birdx += flySpeedX;
-        birdy += flySpeedY;
+        // loop through all birds
+        for (var me = 0; me < NUM_BIRDS; me++) {
 
-        // wrap around and respawn
-        if ((birdx > canvas.width + SPRITE_SIZE + RESPAWN_DISTANCE_OFFSCREEN) ||
-            (birdy > canvas.height + SPRITE_SIZE + RESPAWN_DISTANCE_OFFSCREEN)) { // far enough offscreen?
-            console.log("Respawning bird.");
-            birdx = -SPRITE_SIZE + cameraOffsetX;
-            birdy = Math.random() * canvas.height + cameraOffsetY;
-            flySpeedY = Math.random() - 0.5;
-        }
+            // wrap around and respawn
+            if (!birdx[me] ||
+                !birdy[me] ||
+                (birdx[me] > canvas.width + SPRITE_SIZE + RESPAWN_DISTANCE_OFFSCREEN) ||
+                (birdy[me] > canvas.height + SPRITE_SIZE + RESPAWN_DISTANCE_OFFSCREEN)) { // far enough offscreen?
+                console.log("Respawning bird " + me);
+                birdx[me] = -SPRITE_SIZE + cameraOffsetX - Math.random() * 500;
+                birdy[me] = Math.random() * canvas.height + cameraOffsetY;
+                birdSpeedX[me] = 0.5;
+                birdSpeedY[me] = Math.random() - 0.5;
+            }
 
-        // draw bird
-        canvasContext.drawImage(wildlifeSpritesheet, // see imgLoading.js
-            SPRITE_SIZE * (Math.round(frameCount / ANIM_SPEED) % ANIM_FRAMES), // sx
-            0, // sy
-            SPRITE_SIZE, // sw
-            SPRITE_SIZE, // sh
-            birdx - cameraOffsetX, // dx
-            birdy - cameraOffsetY + Math.cos(frameCount / 100) * altitudeVariance, // dy + wobble
-            SPRITE_SIZE, // dw
-            SPRITE_SIZE); // dh        
-        canvasContext.globalAlpha = 1;
+            // fly forward
+            birdx[me] += birdSpeedX[me];
+            birdy[me] += birdSpeedY[me];
 
+            // draw bird
+            canvasContext.drawImage(wildlifeSpritesheet, // see imgLoading.js
+                SPRITE_SIZE * (Math.round(frameCount / ANIM_SPEED) % ANIM_FRAMES), // sx
+                0, // sy
+                SPRITE_SIZE, // sw
+                SPRITE_SIZE, // sh
+                birdx[me] - cameraOffsetX, // dx
+                birdy[me] - cameraOffsetY + Math.cos(frameCount / 100) * altitudeVariance, // dy + wobble
+                SPRITE_SIZE, // dw
+                SPRITE_SIZE); // dh        
+
+        } // loop through all birds
     }
 
     console.log("Wildlife system init complete.");
