0c2c5b5d62475783f55ddedaf79252ec3641b602
Jeremy Kenyon <lanthos@gmail.com>
2018-06-17 16:34:45 -0700

    merge conflict resolutoin

--- js/Input.js
+++ js/Input.js
@@ -258,8 +258,7 @@ function keyPress(evt) {
         case KEY_USE_TOOL:
             toolKeyPressedThisFrame = true;
             toolKeyHeld = true;
-            player.plantAtFeet();
-            player.workingLand(getTileIndexAtPixelCoord(player.x, player.y), true);
+            player.doActionOnLandTile(getTileIndexAtPixelCoord(player.x, player.y), true);
             if (player.hotbar.slots[player.hotbar.equippedItemIndex].item == items.pickaxe) {
                 pickaxeAnimation.reset();
             }
@@ -325,8 +324,6 @@ function keyPress(evt) {
         case "KeyL":
             resetGame(false);
             break;
-        case "KeyM":
-            
         default:
             // console.log("keycode press is " + evt.keyCode);
             keyUsedByGame = false;
